0c4a5184c866bf07639a98ec9494c56b7a120b7c
LiLiLuL <1354044126@qq.com>
2018-10-26 17:51:16 +0800

    require

--- requireTest/server.js
+++ requireTest/server.js
@@ -2,11 +2,10 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const fs=require('fs');
-const mime=require('mime');
 app.set("view engine","ejs");
 
-app.use(express.static(require('path').join(__dirname, './static')));
+app.use(express.static('./static'));
 app.get('/',(req,res)=>{
     res.render('index');
 })
-app.listen(3000);
+app.listen(3001);
